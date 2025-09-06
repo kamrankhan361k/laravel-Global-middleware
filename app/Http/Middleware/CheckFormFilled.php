ad561990b2351d73cb5a1055a27050c5515d519f
unknown <kamrankhan3.61k@gmail.com>
2025-09-06 11:20:22 +0500

    adding all middleware configuration

---
+++ app/Http/Middleware/CheckFormFilled.php
@@ -0,0 +1,24 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use App\Models\UserForm;
+
+class CheckFormFilled
+{
+    public function handle(Request $request, Closure $next)
+    {
+        // Example: We check if this user already filled the form using email in session
+        if (session()->has('email')) {
+            $email = session('email');
+
+            if (UserForm::where('email', $email)->exists()) {
+                return response()->view('already-filled');
+            }
+        }
+
+        return $next($request);
+    }
+}
